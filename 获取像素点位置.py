d5f2a78b22c6fd220efc25a8c9fc753a9dc9564f
chistni <a2241477745@gmail.com>
2022-09-03 17:57:31 +0800

    first commit

---
+++ 获取像素点位置.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+img=cv2.imread('ba2D_28400_T.tif')
+ 
+def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xy = "%d,%d" % (x, y)
+        print(x,y)
+        cv2.circle(img, (x, y), 2, (0, 0, 255))
+        cv2.putText(img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN,1.0, (0,0,255))
+        cv2.imshow("image", img)
+        
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", on_EVENT_LBUTTONDOWN)
+while(1):
+    cv2.imshow("image", img)
+    key = cv2.waitKey(5) & 0xFF
+    if key == ord('q'):
+        break
+cv2.destroyAllWindows()
